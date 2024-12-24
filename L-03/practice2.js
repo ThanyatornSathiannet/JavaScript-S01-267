79a7490f988186a6ddd2c3e294d242adba51765f
ThanyatornSathiannet <s6606021610079@email.kmutnb.ac.th>
2024-12-24 11:21:29 +0700

    24/12

---
+++ L-03/practice2.js
@@ -0,0 +1,8 @@
+let furniture = ['Table', 'Chairs', 'Couch'];
+
+    for(let char of furniture) {
+        console.log("\n");
+        for(let letter of char) {
+            console.log(letter)
+        }
+    }
